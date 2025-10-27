8def2a986f255a23a1d682e2fc782426a47c190f
EthanT-E <wd22333:bristol.ac.uk>
2025-10-27 15:17:34 +0000

    njited all_energy

--- numba/Leb_numba.py
+++ numba/Leb_numba.py
@@ -173,6 +173,7 @@ def one_energy(arr: np.ndarray, ix: int, iy: int, nmax: int) -> float:
 # =======================================================================
 
 
+@nb.njit
 def all_energy(arr: np.ndarray, nmax: int) -> float:
     """
     Arguments:
