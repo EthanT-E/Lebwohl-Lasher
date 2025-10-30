775d427d0e52672a546d09e70913f727fc12fc07
EthanT-E <wd22333:bristol.ac.uk>
2025-10-30 09:07:34 +0000

    Fixed bug of writing too files as code is too fast

--- avgPlotter.py
+++ avgPlotter.py
@@ -7,13 +7,17 @@ import sys
 
 def main(Folder_Path, column_name):
     paths = listdir(Folder_Path)
+    print(len(paths))
     temps = np.arange(0.1, 1.7, 0.1)
+    print(len(temps))
     avg = []
-    for i in range(0, len(temps)):
+    for i in range(0, len(paths)):
         path = Folder_Path + paths[i]
         df = pd.read_csv(path, skiprows=5)
         avg.append(df[column_name].mean())
 
+    print(len(avg))
+    print(len(temps))
     plt.scatter(x=temps, y=avg)
     plt.show()
 
--- run_temps.sh
+++ run_temps.sh
@@ -3,6 +3,7 @@
 rm ./data/average_order/LL*
 for i in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5,1.6};do
   python $1 1000 20 $i 0
+  sleep 0.5
 done
 mv LL* ./data/average_order/
 python avgPlotter.py "./data/average_order/" "Order"
